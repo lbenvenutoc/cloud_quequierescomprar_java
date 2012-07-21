da878c7e7c1390a77370721e73e13b36b5b8735f
JENNYPRINCIPE <jprincipe@hotmail.com>
2012-07-21 01:40:51 -0500

    Ingreso de registrar empresas, ofertantes...

---
+++ quierescomprar/src/main/java/cloud/quierescomprar/model/Empresas.java
@@ -0,0 +1,82 @@
+package cloud.quierescomprar.model;
+
+/**
+ * Ingreso de Empresas Ofertantes.
+ */
+
+
+public class Empresas {
+	
+	private String codigoEmpresa;
+
+	private String razonSocial;
+
+	private String RUC;
+
+	private String direccion;
+
+	private String tipoEmpresa;
+
+	private String email;
+
+	private String Telefono;
+
+	public String getCodigoEmpresa() {
+		return codigoEmpresa;
+	}
+
+	public void setCodigoEmpresa(String codigoEmpresa) {
+		this.codigoEmpresa = codigoEmpresa;
+	}
+
+	public String getRazonSocial() {
+		return razonSocial;
+	}
+
+	public void setRazonSocial(String razonSocial) {
+		this.razonSocial = razonSocial;
+	}
+
+	public String getRUC() {
+		return RUC;
+	}
+
+	public void setRUC(String rUC) {
+		RUC = rUC;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getTipoEmpresa() {
+		return tipoEmpresa;
+	}
+
+	public void setTipoEmpresa(String tipoEmpresa) {
+		this.tipoEmpresa = tipoEmpresa;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefono() {
+		return Telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		Telefono = telefono;
+	}
+	
+}
+
+	
