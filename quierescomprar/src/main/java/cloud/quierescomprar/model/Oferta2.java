9fe1343053179130b4bcd067d0c088fa69ef6b1d
GeryAnn <geraldine.onofre@gmail.com>
2012-07-20 21:11:47 -0500

    Subiendo la clase oferta y usuario para mi consultar compras

---
+++ quierescomprar/src/main/java/cloud/quierescomprar/model/Oferta2.java
@@ -0,0 +1,89 @@
+package cloud.quierescomprar.model;
+
+import java.util.Date;
+
+public class Oferta2 {
+	private String c_oferta;
+	private String empresa;
+	private String descripcion;
+	private double preciooferta;
+	private double precioregular;
+	private double descuento;
+	private Date fecha_ini;
+	private Date fecha_fin;
+	private String debesaber;
+	private String destacado;
+	private String detalle;
+	
+	public String getC_oferta() {
+		return c_oferta;
+	}
+	public void setC_oferta(String c_oferta) {
+		this.c_oferta = c_oferta;
+	}
+	public String getEmpresa() {
+		return empresa;
+	}
+	public void setEmpresa(String empresa) {
+		this.empresa = empresa;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public double getPreciooferta() {
+		return preciooferta;
+	}
+	public void setPreciooferta(double preciooferta) {
+		this.preciooferta = preciooferta;
+	}
+	public double getPrecioregular() {
+		return precioregular;
+	}
+	public void setPrecioregular(double precioregular) {
+		this.precioregular = precioregular;
+	}
+	public double getDescuento() {
+		return descuento;
+	}
+	public void setDescuento(double descuento) {
+		this.descuento = descuento;
+	}
+	public Date getFecha_ini() {
+		return fecha_ini;
+	}
+	public void setFecha_ini(Date fecha_ini) {
+		this.fecha_ini = fecha_ini;
+	}
+	public Date getFecha_fin() {
+		return fecha_fin;
+	}
+	public void setFecha_fin(Date fecha_fin) {
+		this.fecha_fin = fecha_fin;
+	}
+	public String getDebesaber() {
+		return debesaber;
+	}
+	public void setDebesaber(String debesaber) {
+		this.debesaber = debesaber;
+	}
+	public String getDestacado() {
+		return destacado;
+	}
+	public void setDestacado(String destacado) {
+		this.destacado = destacado;
+	}
+	public String getDetalle() {
+		return detalle;
+	}
+	public void setDetalle(String detalle) {
+		this.detalle = detalle;
+	}
+	
+	
+	
+	
+	
+}
