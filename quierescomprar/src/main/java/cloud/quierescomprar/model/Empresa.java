aedc32d9aa02cfd4bd7e07166abebcc0b34cebbf
JENNYPRINCIPE <jprincipe@hotmail.com>
2012-07-21 09:16:12 -0500

    Modificando la clase Empresa

---
+++ quierescomprar/src/main/java/cloud/quierescomprar/model/Empresa.java
@@ -0,0 +1,77 @@
+package cloud.quierescomprar.model;
+
+public class Empresa {
+
+	
+	private String c_Empresa;
+
+	private String razonSocial;
+
+	private String RUC;
+
+	private String direccion;
+
+	private String tipoEmpresa;
+
+	private String email;
+
+	private String Telefono;
+
+	public String getC_Empresa() {
+		return c_Empresa;
+	}
+
+	public void setC_Empresa(String c_Empresa) {
+		this.c_Empresa = c_Empresa;
+	}
+
+	public String getRazonSocial() {
+		return razonSocial;
+	}
+
+	public void setRazonSocial(String razonSocial) {
+		this.razonSocial = razonSocial;
+	}
+
+	public String getRUC() {
+		return RUC;
+	}
+
+	public void setRUC(String rUC) {
+		RUC = rUC;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getTipoEmpresa() {
+		return tipoEmpresa;
+	}
+
+	public void setTipoEmpresa(String tipoEmpresa) {
+		this.tipoEmpresa = tipoEmpresa;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefono() {
+		return Telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		Telefono = telefono;
+	}
+
+
+}
