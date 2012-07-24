2b8eb17dcb565d56d398f72b6719092dc891f80b
JennyPrincipe <jenprincipe@hotmail.com>
2012-07-23 21:15:26 -0500

    Creando la interfaz de EmpresaDao

---
+++ quierescomprar/src/main/java/cloud/quierescomprar/dao/EmpresaDao.java
@@ -0,0 +1,12 @@
+package cloud.quierescomprar.dao;
+
+import java.util.List;
+
+import cloud.quierescomprar.model.Empresa;;
+
+public interface EmpresaDao {
+
+	public List<Empresa> listaEmpresa();
+		
+	
+}
